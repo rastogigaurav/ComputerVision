31be46e5dbcc6d21a6cb19c544bfa0d8dc42979b
Gaurav Rastogi <grastogi@bbmtek.com>
2018-04-16 23:10:04 +0530

    Created folder structure w.r.t. MVVM + Clean Architecture, added App Icon & Launch Screen, added Main tabbar controller and two initial view controller

---
+++ ComputerVision/ComputerVision/Views/NativeViewController.swift
@@ -0,0 +1,27 @@
+//
+//  NativeViewController.swift
+//  ComputerVision
+//
+//  Created by Gaurav Rastogi on 4/16/18.
+//  Copyright © 2018 Gaurav Rastogi. All rights reserved.
+//
+
+import UIKit
+
+class NativeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func openCameraTapped(_ sender: UIButton) {
+    }
+    
+}
--- ComputerVision/ComputerVision/Views/OpenCVViewController.swift
+++ ComputerVision/ComputerVision/Views/OpenCVViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  OpenCVViewController.swift
 //  ComputerVision
 //
 //  Created by Gaurav Rastogi on 4/16/18.
@@ -8,18 +8,19 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class OpenCVViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    @IBAction func openCameraTapped(_ sender: UIButton) {
+    }
 }
-
