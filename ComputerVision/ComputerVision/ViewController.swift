ffa6586a4ccead0db2504e3097255335f2d05c03
Gaurav Rastogi <grastogi@bbmtek.com>
2018-04-16 21:24:09 +0530

    Added initial project to the repository

---
+++ ComputerVision/ComputerVision/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ComputerVision
+//
+//  Created by Gaurav Rastogi on 4/16/18.
+//  Copyright © 2018 Gaurav Rastogi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ ComputerVision/ComputerVisionTests/ComputerVisionTests.swift
@@ -0,0 +1,36 @@
+//
+//  ComputerVisionTests.swift
+//  ComputerVisionTests
+//
+//  Created by Gaurav Rastogi on 4/16/18.
+//  Copyright © 2018 Gaurav Rastogi. All rights reserved.
+//
+
+import XCTest
+@testable import ComputerVision
+
+class ComputerVisionTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
